e30a6c8a96da01adf53e7df4ff49ec3fd842ce6e
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2023-06-14 18:04:23 +0900

    Update EnemyMove.cs

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    Rigidbody2D rigid;
+    Rigidbody2D rigid;d
     Animator anim;
     SpriteRenderer spriteRenderer;
     CapsuleCollider2D collider;
